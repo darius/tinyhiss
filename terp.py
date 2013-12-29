7a732c48df84fbd8f4287f713e6e3df241baaefe
Darius Bacon <darius@wry.me>
2013-12-29 04:07:31 -0300

    more-or-less readable tracebacks

--- terp.py
+++ terp.py
@@ -5,11 +5,23 @@ AST interpreter
 from collections import namedtuple
 
 def trampoline(state):
+#    traceback(state)
     k, value = state
     while k is not None:
         fn, free_var, k = k
         k, value = fn(value, free_var, k)
     return value
+
+def traceback(state):
+    k, value = state
+    print ':', value
+    while k:
+        fn, free_var, k = k
+        if isinstance(free_var, tuple) and free_var:
+            for i, element in enumerate(free_var):
+                print '%-18s %r' % (('' if i else fn.__name__), element)
+        else:
+            print '%-18s %r' % (fn.__name__, free_var)
 
 def call(receiver, selector, args, k):
     return get_class(receiver).get_method(selector)(receiver, args, k)
@@ -46,6 +58,12 @@ class Class(namedtuple('_Class', 'methods ivars')):
         self.methods[selector] = method
     def make(self):
         return Thing(self, [None] * len(self.ivars))
+    def __repr__(self):
+        for k, v in global_env.items():
+            if self is v:
+                return k
+        return '<<Class %s | %s>>' % (' '.join(self.ivars),
+                                      self.methods)
 
 def Method(params, local_vars, expr):
     return Block(None, None, Code(params, local_vars, expr))
@@ -57,6 +75,11 @@ class Block(namedtuple('_Block', 'receiver env code')):
             rib[var] = None
         real_receiver = self.receiver or receiver
         return self.code.expr.eval(real_receiver, Env(rib, self.env), k)
+    def __repr__(self):
+        if self.receiver is None and self.env is None:
+            return repr(self.code)
+        else:
+            return 'Block(%r, %r, %r)' % (self.receiver, self.env, self.code)
 
 class Env(namedtuple('_Env', 'rib container')):
     def get(self, key):
@@ -70,6 +93,8 @@ class Env(namedtuple('_Env', 'rib container')):
             return self.container.find(key)
         else:
             raise KeyError(key)
+    def __repr__(self):
+        return 'Env(%r, %r)' % (self.rib, self.container)
 
 def new_method(receiver, arguments, k):
     return k, receiver.make()
@@ -95,14 +120,24 @@ def as_number(thing):
 class Self(namedtuple('_Self', '')):
     def eval(self, receiver, env, k):
         return k, receiver
+    def __repr__(self):
+        return 'I'
 
 class Constant(namedtuple('_Constant', 'value')):
     def eval(self, receiver, env, k):
         return k, self.value
+    def __repr__(self):
+        return repr(self.value)
 
 class Code(namedtuple('_Code', 'params locals expr')):
     def eval(self, receiver, env, k):
         return k, Block(receiver, env, self)
+    def __repr__(self):
+        params = ' '.join(':'+param for param in self.params)
+        if params: params += ' | '
+        locs = ' '.join(self.locals)
+        if locs: locs = '|%s| ' % locs
+        return '{%s%s%r}' % (params, locs, self.expr)
 
 def with_key(key, thunk):
     try: return thunk()
@@ -112,14 +147,20 @@ def with_key(key, thunk):
 class GlobalGet(namedtuple('_GlobalGet', 'name')):
     def eval(self, receiver, env, k):
         return with_key(self.name, lambda: (k, global_env[self.name]))
+    def __repr__(self):
+        return str(self.name)
 
 class LocalGet(namedtuple('_LocalGet', 'name')):
     def eval(self, receiver, env, k):
         return with_key(self.name, lambda: (k, env.get(self.name)))
+    def __repr__(self):
+        return str(self.name)
 
 class LocalPut(namedtuple('_LocalPut', 'name expr')):
     def eval(self, receiver, env, k):
         return self.expr.eval(receiver, env, (putting_k, (self.name, env), k))
+    def __repr__(self):
+        return '%s <- %r' % (self.name, self.expr)
 
 def putting_k(value, (name, thing), k):
     with_key(name, lambda: thing.put(name, value))
@@ -129,11 +170,15 @@ class SlotGet(namedtuple('_SlotGet', 'name')):
     def eval(self, receiver, env, k):
         return with_key(self.name,
                         lambda: (k, as_slottable(receiver).get(self.name)))
+    def __repr__(self):
+        return 'my ' + str(self.name)
 
 class SlotPut(namedtuple('_SlotPut', 'name expr')):
     def eval(self, receiver, env, k):
         return self.expr.eval(receiver, env,
                               (putting_k, (self.name, as_slottable(receiver)), k))
+    def __repr__(self):
+        return 'my %s <- %r' % (self.name, self.expr)
 
 def as_slottable(thing):
     if not isinstance(thing, Thing):
@@ -146,6 +191,8 @@ class Cascade(namedtuple('_Cascade', 'subject selector operands')):
     def eval(self, receiver, env, k):
         return self.subject.eval(receiver, env,
                                  (cascade_evrands_k, (self, receiver, env), k))
+    def __repr__(self):
+        return send_repr(self, ';')
 
 def cascade_evrands_k(subject, (self, receiver, env), k):
     return evrands(self.operands, receiver, env,
@@ -159,6 +206,18 @@ class Send(namedtuple('_Send', 'subject selector operands')):
     def eval(self, receiver, env, k):
         return self.subject.eval(receiver, env,
                                  (evrands_k, (self, receiver, env), k))
+    def __repr__(self):
+        return send_repr(self)
+
+def send_repr(self, sep=''):
+    subject = repr(self.subject) + sep
+    if len(self.operands) == 0:
+        return '(%s %s)' % (subject, self.selector)
+    elif len(self.operands) == 1:
+        return '(%s %s %r)' % (subject, self.selector, self.operands[0])
+    else:
+        pairs = zip(self.selector.split(':'), self.operands)
+        return '(%s%s)' % (subject, ''.join(' %s: %r' % pair for pair in pairs))
 
 def evrands_k(subject, (self, receiver, env), k):
     return evrands(self.operands, receiver, env,
@@ -183,6 +242,8 @@ def evrands_cons_k(vals, val, k):
 class Then(namedtuple('_Then', 'expr1 expr2')):
     def eval(self, receiver, env, k):
         return self.expr1.eval(receiver, env, (then_k, (self, receiver, env), k))
+    def __repr__(self):
+        return '%r. %r' % (self.expr1, self.expr2)
 
 def then_k(_, (self, receiver, env), k):
     return self.expr2.eval(receiver, env, k)
