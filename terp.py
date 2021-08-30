3df7978dc87a63d572981f2d864ec2766f0b2335
darius <withal@gmail.com>
2021-08-29 22:34:29 -0500

    rename some vars

--- terp.py
+++ terp.py
@@ -93,11 +93,11 @@ class Method(namedtuple('_Method', 'source code')):
     def __repr__(self):
         return repr(self.code)
 
-class Block(namedtuple('_Block', 'receiver env code')):
+class Block(namedtuple('_Block', 'me env code')):
     def enter(self, arguments, k):
-        return self.code.enter(self.receiver, arguments, self.env, k)
+        return self.code.enter(self.me, arguments, self.env, k)
     def __repr__(self):
-        return 'Block(%r, %r, %r)' % (self.receiver, self.env, self.code)
+        return 'Block(%r, %r, %r)' % (self.me, self.env, self.code)
 
 class Env(namedtuple('_Env', 'rib container')):
     # The following two methods are meant only for the global env and
@@ -201,25 +201,25 @@ array_methods = {
 array_class = Class(array_methods, ())
 
 class Self(namedtuple('_Self', '')):
-    def eval(self, receiver, env, k):
-        return k, receiver
+    def eval(self, me, env, k):
+        return k, me
     def __repr__(self):
         return 'I'
 
 class Constant(namedtuple('_Constant', 'value')):
-    def eval(self, receiver, env, k):
+    def eval(self, me, env, k):
         return k, self.value
     def __repr__(self):
         return repr(self.value)
 
 class Code(namedtuple('_Code', 'params locals expr')):
-    def eval(self, receiver, env, k):
-        return k, Block(receiver, env, self)
-    def enter(self, receiver, arguments, parent_env, k):
+    def eval(self, me, env, k):
+        return k, Block(me, env, self)
+    def enter(self, me, arguments, parent_env, k):
         rib = dict(zip(self.params, arguments))
         for name in self.locals:
             rib[name] = None
-        return self.expr.eval(receiver, Env(rib, parent_env), k)
+        return self.expr.eval(me, Env(rib, parent_env), k)
     def __repr__(self):
         params = ' '.join(':'+param for param in self.params)
         if params: params += ' | '
@@ -233,20 +233,20 @@ def with_key(key, thunk):
         raise Exception("Unbound", key)
 
 class GlobalGet(namedtuple('_GlobalGet', 'name')):
-    def eval(self, receiver, env, k):
+    def eval(self, me, env, k):
         return with_key(self.name, lambda: (k, global_env.get(self.name)))
     def __repr__(self):
         return str(self.name)
 
 class LocalGet(namedtuple('_LocalGet', 'name')):
-    def eval(self, receiver, env, k):
+    def eval(self, me, env, k):
         return with_key(self.name, lambda: (k, env.get(self.name)))
     def __repr__(self):
         return str(self.name)
 
 class LocalPut(namedtuple('_LocalPut', 'name expr')):
-    def eval(self, receiver, env, k):
-        return self.expr.eval(receiver, env, (putting_k, (self.name, env), k))
+    def eval(self, me, env, k):
+        return self.expr.eval(me, env, (putting_k, (self.name, env), k))
     def __repr__(self):
         return '%s <- %r' % (self.name, self.expr)
 
@@ -255,16 +255,16 @@ def putting_k(value, (name, thing), k):
     return k, value
 
 class SlotGet(namedtuple('_SlotGet', 'name')):
-    def eval(self, receiver, env, k):
+    def eval(self, me, env, k):
         return with_key(self.name,
-                        lambda: (k, as_slottable(receiver).get(self.name)))
+                        lambda: (k, as_slottable(me).get(self.name)))
     def __repr__(self):
         return 'my ' + str(self.name)
 
 class SlotPut(namedtuple('_SlotPut', 'name expr')):
-    def eval(self, receiver, env, k):
-        return self.expr.eval(receiver, env,
-                              (putting_k, (self.name, as_slottable(receiver)), k))
+    def eval(self, me, env, k):
+        return self.expr.eval(me, env,
+                              (putting_k, (self.name, as_slottable(me)), k))
     def __repr__(self):
         return 'my %s <- %r' % (self.name, self.expr)
 
@@ -276,24 +276,24 @@ def as_slottable(thing):
 global_env = Env({}, None)
 
 class Cascade(namedtuple('_Cascade', 'subject selector operands')):
-    def eval(self, receiver, env, k):
-        return self.subject.eval(receiver, env,
-                                 (cascade_evrands_k, (self, receiver, env), k))
+    def eval(self, me, env, k):
+        return self.subject.eval(me, env,
+                                 (cascade_evrands_k, (self, me, env), k))
     def __repr__(self):
         return send_repr(self, ';')
 
-def cascade_evrands_k(subject, (self, receiver, env), k):
-    return evrands(self.operands, receiver, env,
-                   (call_k, (subject, self),
+def cascade_evrands_k(subject, (cascade, me, env), k):
+    return evrands(cascade.operands, me, env,
+                   (call_k, (subject, cascade),
                     (ignore_k, subject, k)))
 
 def ignore_k(_, result, k):
     return k, result
 
 class Send(namedtuple('_Send', 'subject selector operands')):
-    def eval(self, receiver, env, k):
-        return self.subject.eval(receiver, env,
-                                 (evrands_k, (self, receiver, env), k))
+    def eval(self, me, env, k):
+        return self.subject.eval(me, env,
+                                 (evrands_k, (self, me, env), k))
     def __repr__(self):
         return send_repr(self)
 
@@ -307,34 +307,34 @@ def send_repr(self, sep=''):
         pairs = zip(self.selector.split(':'), self.operands)
         return '(%s%s)' % (subject, ''.join(' %s: %r' % pair for pair in pairs))
 
-def evrands_k(subject, (self, receiver, env), k):
-    return evrands(self.operands, receiver, env,
-                   (call_k, (subject, self), k))
+def evrands_k(subject, (send, me, env), k):
+    return evrands(send.operands, me, env,
+                   (call_k, (subject, send), k))
 
-def call_k(args, (subject, self), k):
-    return call(subject, self.selector, args, k)
+def call_k(args, (subject, send_or_cascade), k):
+    return call(subject, send_or_cascade.selector, args, k)
 
-def evrands(operands, receiver, env, k):
+def evrands(operands, me, env, k):
     if not operands:
         return k, ()
     else:
-        return operands[0].eval(receiver, env,
-                                (evrands_more_k, (operands[1:], receiver, env), k))
+        return operands[0].eval(me, env,
+                                (evrands_more_k, (operands[1:], me, env), k))
 
-def evrands_more_k(val, (operands, receiver, env), k):
-    return evrands(operands, receiver, env, (evrands_cons_k, val, k))
+def evrands_more_k(val, (operands, me, env), k):
+    return evrands(operands, me, env, (evrands_cons_k, val, k))
 
 def evrands_cons_k(vals, val, k):
     return k, (val,)+vals
 
 class Then(namedtuple('_Then', 'expr1 expr2')):
-    def eval(self, receiver, env, k):
-        return self.expr1.eval(receiver, env, (then_k, (self, receiver, env), k))
+    def eval(self, me, env, k):
+        return self.expr1.eval(me, env, (then_k, (self, me, env), k))
     def __repr__(self):
         return '%r. %r' % (self.expr1, self.expr2)
 
-def then_k(_, (self, receiver, env), k):
-    return self.expr2.eval(receiver, env, k)
+def then_k(_, (then_, me, env), k):
+    return then_.expr2.eval(me, env, k)
 
 true_class = Class({}, ())   # Filled in at startup
 false_class = Class({}, ())  # ditto
